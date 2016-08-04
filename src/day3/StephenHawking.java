221e192dc523fa599577d4f61699fd0a143e7f5a
Skylar <gerings17@bishops.com>
2016-08-04 12:06:22 -0700

    t
    egw

--- src/day3/StephenHawking.java
+++ src/day3/StephenHawking.java
@@ -2,11 +2,25 @@ package day3;
 
 import java.io.IOException;
 
-public class StephenHawking {
+import javax.swing.JOptionPane;
 
+public class StephenHawking {
+public static void main(String[] args) {	
+	 {
+	
+}
+for (int i = 0; i < 8; i++) {
+	
+}	 
+String answer=JOptionPane.showInputDialog("Hello there persons, please give a sentence");	
+speak(answer);
+}
 	// 1. make a main method and put steps 2, 3 & 4 inside it
+
 	// 2. ask the user for a sentence
+
 	// 3. call the speak method below and send it the sentence
+
 	// 4. repeat steps 2 and 3 a lot of times
 
 	/* Don’t change this…. */
